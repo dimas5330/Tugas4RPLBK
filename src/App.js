8a6e04c5fa77a6e2e79a848f14cb9ce6e120054f
Dimas Seto Rizky Goenardi <goenardid@gmail.com>
2022-11-03 18:31:22 +0700

    nambah counter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Effect from './learn-useEffect/index';
+import Counter from './learn-useState/counter';
 
 function App() {
   return (
@@ -13,10 +14,14 @@ function App() {
             <li className='li'>
               <Link to="/">Use State</Link>
             </li>
+            <li className='li'>
+              <Link to='/effect'>Use Effect</Link>
+            </li>
           </ul>
         </nav>
         <Routes>
-          <Route path='/' exact element = {<Effect/>}/>
+        <Route path='/' exact element = {<Counter/>}/>
+        <Route path='/effect' exact element = {<Effect/>}/>
         </Routes>
       </div>
     </Router>
